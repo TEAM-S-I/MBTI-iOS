c0645517eb009350c5da359658e9df76a345601f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-18 11:22:26 +0900

    Remove :: Main View

---
+++ mbti/mbti/Source/Feature/LaunchScreen/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/18/23.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        HStack(alignment: .center) {
+            VStack(alignment: .center) {
+                MbtiLogoImage(type: .big)
+            }
+        }
+    }
+}
