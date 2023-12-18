137108e6b2b7abf82a2d5c6013b557460fec022f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-18 10:35:05 +0900

    Add :: Match View

--- mbti/mbti/Source/Feature/Match/MatchView.swift
+++ mbti/mbti/Source/Feature/Match/MatchView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct MatchView: View {
     var body: some View {
-        Text("match view")
+        VStack(spacing: 0) {
+            MbtiLogoImage(type: .normal)
+            Text("팀 매칭")
+                .applyFontStyle(.title)
+                .padding(.top, 12)
+            MbtiButton("MBTI 팀 매칭 시작") {
+                print("start")
+            }
+            .padding(.top, 32)
+            Button {
+                print("match log")
+            } label: {
+                Text("매칭 기록 보기")
+                    .foregroundColor(.gray500)
+                    .applyFontStyle(.label)
+                    .underline()
+            }
+            .padding(.top, 24)
+        }
     }
 }
 
