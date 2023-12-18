eb1a4e88f2d608fb55ae1cca19836026889eaacb
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-18 10:59:49 +0900

    Add :: Check View

--- mbti/mbti/Source/Feature/Check/CheckView.swift
+++ mbti/mbti/Source/Feature/Check/CheckView.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 struct CheckView: View {
     var body: some View {
-        Text("check view")
+        VStack {
+            Text("내 팀의 장단점은 무엇일까?")
+                .applyFontStyle(.title)
+            Text("MBTI로 알아보는 나의 팀 상태")
+                .foregroundColor(.main300)
+                .applyFontStyle(.body)
+                .padding(.top, 4)
+            MbtiButton("팀 진단 시작하기") {
+                print("let's start to check")
+            }
+            .padding(.top, 24)
+        }
     }
 }
