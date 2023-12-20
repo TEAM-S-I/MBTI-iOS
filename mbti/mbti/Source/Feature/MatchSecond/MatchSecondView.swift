608e3a348d84636ee17c363f4afc68cd322d9ba2
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-20 14:36:19 +0900

    Add :: Match Second View

--- mbti/mbti/Source/Feature/MatchSecond/MatchSecondView.swift
+++ mbti/mbti/Source/Feature/MatchSecond/MatchSecondView.swift
@@ -10,37 +10,48 @@ import SwiftUI
 struct MatchSecondView: View {
     
     @Environment(\.dismiss) private var dismiss
+    @State var sliderValue = 1.0
     
     var body: some View {
-        HStack {
-            Spacer()
-            VStack {
+        GeometryReader { geo in
+            HStack {
                 Spacer()
                 VStack {
-                    Text("총 N명이시네요!")
+                    Text("총 N명이시네요!\n마지막으로 팀 당 인원을 정해주세요")
                         .applyFontStyle(.title)
-                        .padding(.top, 100)
-                        .padding(.top, 64)
-                    Text("마지막으로 팀 당 인원을 정해주세요")
-                        .applyFontStyle(.title)
-                        .padding(.top, 100)
-                        .padding(.top, 64)
+                        .padding(.top, geo.size.height / 5)
+                    Text("\(Int(sliderValue))명")
+                        .applyFontStyle(.subtitle)
+                        .padding(.top, geo.size.height / 6)
+                    HStack {
+                        Text("1")
+                            .applyFontStyle(.body)
+                        Slider(value: $sliderValue, in: 1...7, step: 1)
+                            .tint(.main800)
+                            .accentColor(.main300)
+                        Text("7")
+                            .applyFontStyle(.body)
+                    }
+                    .padding(.top, 8)
+                    .padding(.horizontal, 24)
+                    Text("\(Int(ceil(7/sliderValue)))팀으로 나눠져요")
+                        .foregroundColor(.main300)
+                        .applyFontStyle(.body)
+                    Spacer()
+                    MbtiTransparentButton("다음 단계로") {
+                        print("NextLevel")
+                    }
+                    .padding(.vertical, 12)
                 }
-                 
                 Spacer()
-                MbtiTransparentButton("다음 단계로") {
-                    print("NextLevel")
-                }
-                .padding(.vertical, 12)
             }
-            Spacer()
+            .addMbtiLogo()
+            .addBackButton {
+                dismiss()
+            }
+            .navigationBarBackButtonHidden()
+            .background(Color.main100)
         }
-        .addMbtiLogo()
-        .addBackButton {
-            dismiss()
-        }
-        .navigationBarBackButtonHidden()
-        .background(Color.main100)
     }
 }
 
