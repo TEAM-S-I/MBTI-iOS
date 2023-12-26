a8406fa6113cea2b9ad035333bde7c1a62cc0513
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 21:38:54 +0900

    Add :: PacResponse

---
+++ mbti/mbti/Source/DTO/Response/PacResponse.swift
@@ -0,0 +1,13 @@
+//
+//  PacResponse.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/26/23.
+//
+
+import Foundation
+
+struct PacResponse: Decodable {
+    let team: String
+    let members: [PacMemberResponse]
+}
