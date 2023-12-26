e656bec91391f710f948413fd802631ae5821cd1
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 21:36:24 +0900

    Add :: TeamMemberResponse

---
+++ mbti/mbti/Source/DTO/Response/TeamMemberResponse.swift
@@ -0,0 +1,24 @@
+//
+//  TeamMemberResponse.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/26/23.
+//
+
+import Foundation
+
+struct TeamMemberResponse: Decodable {
+    let name: String
+    let mbti: String
+    let role: String
+    let description: String
+}
+
+extension TeamMemberResponse {
+    func toModel() -> TeamMemberModel {
+        return TeamMemberModel(name: self.name,
+                               mbti: MbtiType.getType(self.mbti) ?? .enfj,
+                               role: self.role,
+                               description: self.description)
+    }
+}
