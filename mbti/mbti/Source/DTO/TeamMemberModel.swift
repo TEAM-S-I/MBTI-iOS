9d166acd0c88b8d52055b096607fc744a5fc9c95
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 21:36:29 +0900

    Add :: TeamMemberModel

---
+++ mbti/mbti/Source/DTO/TeamMemberModel.swift
@@ -0,0 +1,16 @@
+//
+//  TeamMemberModel.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/26/23.
+//
+
+import Foundation
+
+
+struct TeamMemberModel {
+    let name: String
+    let mbti: MbtiType
+    let role: String
+    let description: String
+}
