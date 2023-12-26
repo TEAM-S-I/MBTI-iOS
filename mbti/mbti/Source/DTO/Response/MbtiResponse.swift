31b509285cb6430a236e53c5db42a3238f48ab9f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 21:21:21 +0900

    Add :: MbtiResponse & extension can traslate to model

--- mbti/mbti/Source/DTO/Response/MbtiResponse.swift
+++ mbti/mbti/Source/DTO/Response/MbtiResponse.swift
@@ -5,4 +5,18 @@
 //  Created by dgsw8th71 on 12/26/23.
 //
 
-import Foundation
+struct MbtiResponse: Decodable {
+    let name: String
+    let mbti: String
+}
+
+extension MbtiResponse {
+    func toModel() -> MbtiModel {
+        let mbtiType = MbtiType.allCases.filter { i in
+            i.rawValue == self.mbti
+        }[0]
+        
+        return MbtiModel(name: self.name, mbti: mbtiType)
+    }
+}
+
