d732ff199e8a1c83c2f222241f5061185bd8a8a7
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 21:36:10 +0900

    Add :: TeamResponse

---
+++ mbti/mbti/Source/DTO/Response/TeamResposne.swift
@@ -0,0 +1,12 @@
+//
+//  TeamResposne.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/26/23.
+//
+
+import Foundation
+
+struct TeamResposne: Decodable {
+    let team: [TeamDescriptionResponse]
+}
