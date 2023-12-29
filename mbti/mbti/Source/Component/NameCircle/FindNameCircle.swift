7975adb7d64b417ae590e30d091a40deb53d2a36
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-29 12:47:31 +0900

    Add :: FindNameCircle

---
+++ mbti/mbti/Source/Component/NameCircle/FindNameCircle.swift
@@ -0,0 +1,18 @@
+//
+//  FindNameCircle.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/29/23.
+//
+
+import SwiftUI
+
+struct FindNameCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FindNameCircle()
+}
