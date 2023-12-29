dd864f517fb1af76e792d27ced2a05ea99b85191
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-29 16:00:39 +0900

    Add :: navigation util

---
+++ mbti/mbti/Source/Util/NavigationUtil.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationUtil.swift
+//  mbti
+//
+//  Created by dgsw8th71 on 12/29/23.
+//
+
+import SwiftUI
+
+struct NavigationUtil {
+    
+    static func popToRootView() {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .compactMap({$0 as? UIWindowScene})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        findNavigationController(viewController: keyWindow?.rootViewController)?
+            .popToRootViewController(animated: true)
+        
+    }
+    
+    static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
+        guard let viewController = viewController else {
+            return nil
+        }
+        
+        if let navigationController = viewController as? UINavigationController {
+            return navigationController
+        }
+        
+        for childViewController in viewController.children {
+            return findNavigationController(viewController: childViewController)
+        }
+        return nil
+    }
+}
