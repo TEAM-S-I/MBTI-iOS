a7451d455659f43e1331057ef5873fd9574268ef
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-29 12:39:40 +0900

    Add :: apply view by side effect in match result

--- mbti/mbti/Source/Feature/MatchResult/MatchResultView.swift
+++ mbti/mbti/Source/Feature/MatchResult/MatchResultView.swift
@@ -18,17 +18,32 @@ struct MatchResultView: View {
     
     var body: some View {
         VStack {
-            Text("매칭 결과")
-                .applyFontStyle(.title)
-                .padding(.top, 72)
-            Text("광고를 보면 팀원들의 역할과 특징을 알 수 있어요!")
-                .padding(.top, 12)
-            ScrollView {
-                VStack {
-//                    ForEach()
-                }
+            switch viewModel.sideEffect {
+            case .Loading:
+                LoadingView()
+            case .Success:
+                CompleteView()
+            case .Result:
+                matchResult
+            case .Fail:
+                LoadingView()
             }
+            
         }
         .addMbtiLogo()
+    }
+    
+    @ViewBuilder
+    var matchResult: some View {
+        Text("매칭 결과")
+            .applyFontStyle(.title)
+            .padding(.top, 72)
+        Text("광고를 보면 팀원들의 역할과 특징을 알 수 있어요!")
+            .padding(.top, 12)
+        ScrollView {
+            VStack {
+//                    ForEach()
+            }
+        }
     }
 }
