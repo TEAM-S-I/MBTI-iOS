1028930ae296259162361a2e30bbba6b8647518b
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-29 13:32:07 +0900

    Fix :: loading circle

--- mbti/mbti/Source/Feature/Loading/LoadingView.swift
+++ mbti/mbti/Source/Feature/Loading/LoadingView.swift
@@ -11,7 +11,7 @@ struct LoadingView: View {
     var body: some View {
         VStack {
             Circle()
-                .background(Color.main800)
+                .foregroundColor(.main800)
                 .frame(width: 78, height: 78)
             Text("AI가 최적의 팀을\n만드는 중이에요")
                 .applyFontStyle(.title)
