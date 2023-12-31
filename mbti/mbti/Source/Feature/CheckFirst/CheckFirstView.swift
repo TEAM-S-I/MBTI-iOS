7de44d602e1ffee167889931996078330ff0634f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-01 00:48:08 +0900

    Add :: check first view

--- mbti/mbti/Source/Feature/CheckFirst/CheckFirstView.swift
+++ mbti/mbti/Source/Feature/CheckFirst/CheckFirstView.swift
@@ -10,38 +10,79 @@ import SwiftUI
 struct CheckFirstView: View {
     
     @Environment(\.dismiss) private var dismiss
+    @State var isAddActive = false
+    @State var isWarnActive = false
+    @State var name = ""
+    @State var mbti: MbtiType? = nil
+    @State var data: [MbtiDTO] = []
     
-    var data: [MbtiDTO] = []
     var body: some View {
-        HStack {
-            Spacer()
-            VStack {
+        ZStack {
+            HStack {
                 Spacer()
-                Text("진단할 팀원을 추가해 주세요")
-                    .applyFontStyle(.title)
-                    .padding(.top, 100)
-                MbtiGrid(data: data) {
-                    
-                }
-                    .padding(.top, 64)
-                Spacer()
-                NavigationLink {
-//                    MatchSecondView(data: viewModel.data)
-                } label: {
-                    MbtiTransparentButton("다음 단계로") {
-                        
+                VStack {
+                    Spacer()
+                    Text("진단할 팀원을 추가해 주세요")
+                        .applyFontStyle(.title)
+                        .padding(.top, 100)
+                    MbtiGrid(data: data) {
+                        isAddActive = true
                     }
-                    .disabled(true)
+                    .padding(.top, 64)
+                    Spacer()
+                    NavigationLink {
+                        MatchSecondView(data: $data)
+                    } label: {
+                        MbtiTransparentButton("진단시작!") {
+                            isWarnActive = true
+                        }
+                        .disabled(data.count >= 2)
+                    }
+                    .padding(.vertical, 12)
                 }
-                .padding(.vertical, 12)
+                Spacer()
             }
-            Spacer()
+            .addMbtiLogo()
+            .addBackButton {
+                dismiss()
+            }
+            .navigationBarBackButtonHidden()
+            .background(Color.main100)
+            if isAddActive {
+                MbtiDialog(isActive: $isAddActive) {
+                    Text("팀원 정보 입력")
+                        .applyFontStyle(.subtitle)
+                        .padding(.leading, 4)
+                        .toLeading()
+                    MbtiTextField("이름을 입력해 주세요", text: $name, type: .topRadius)
+                        .padding(.top, 12)
+                    MbtiDropDown(choicedElement: $mbti, type: .bottomRadius)
+                    MbtiTransparentButton("추가 완료", fontStyle: .body) {
+                        guard name.isEmpty || mbti == nil else {
+                            data.append(MbtiDTO(name: name, mbti: mbti ?? .enfj))
+                            name = ""
+                            mbti = nil
+                            isAddActive = false
+                            return
+                        }
+                    }
+                    .padding(.top, 16)
+                    .padding(.bottom, 4)
+                }
+            }
+            if isWarnActive {
+                MbtiDialog(isActive: $isWarnActive, isCloseButton: false) {
+                    Text("팀원을 두 명 이상 추가해 주세요!")
+                        .applyFontStyle(.subtitle)
+                        .padding(.leading, 4)
+                        .toLeading()
+                    
+                    MbtiTransparentButton("닫기", fontStyle: .body) {
+                        isWarnActive = false
+                    }
+                    .padding(.top, 36)
+                }
+            }
         }
-        .addMbtiLogo()
-        .addBackButton {
-            dismiss()
-        }
-        .navigationBarBackButtonHidden()
-        .background(Color.main100)
     }
 }
