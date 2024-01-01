21e1d97139b97b4e3968f57ee5967d6a1029a26f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-01 17:33:40 +0900

    Add :: to select mbti alert

--- mbti/mbti/Source/Feature/Find/FindView.swift
+++ mbti/mbti/Source/Feature/Find/FindView.swift
@@ -15,44 +15,42 @@ struct FindView: View {
     
     var body: some View {
         HStack {
-            ZStack {
+            Spacer()
+            VStack(spacing: 0) {
                 Spacer()
-                VStack(spacing: 0) {
-                    Spacer()
-                    Text("팀플할 때 나랑 맞는 MBTI는?")
-                        .applyFontStyle(.title)
-                    Text("자신의 MBTI를 입력해 보세요")
-                        .foregroundColor(.main300)
-                        .applyFontStyle(.body)
-                    MbtiDropDown(choicedElement: $mbti)
-                        .padding(.top, 24)
-                    NavigationLink(isActive: $isFindResult) {
-                        FindResultView(title: "당신(\(mbti?.rawValue ?? "..."))과 가장 맞는 팀원은 바로...",
-                                       findData: mbti?.pacDTO
-                        )
-                    } label: {
-                        
-                    }
-                    MbtiButton("찾기") {
-                        if mbti == nil {
-                            isWarning = true
-                        } else {
-                            isFindResult = true
-                        }
-                    }
-                    .padding(.top, 8)
+                Text("팀플할 때 나랑 맞는 MBTI는?")
+                    .applyFontStyle(.title)
+                Text("자신의 MBTI를 입력해 보세요")
+                    .foregroundColor(.main300)
+                    .applyFontStyle(.body)
+                MbtiDropDown(choicedElement: $mbti)
+                    .padding(.top, 24)
+                NavigationLink(isActive: $isFindResult) {
+                    FindResultView(title: "당신(\(mbti?.rawValue ?? "..."))과 가장 맞는 팀원은 바로...",
+                                   findData: mbti?.pacDTO
+                    )
+                } label: {
                     
-                    Spacer()
                 }
-                Spacer()
-                MbtiDialog(isActive: $isWarning) {
+                MbtiButton("찾기") {
+                    if mbti == nil {
+                        isWarning = true
+                    } else {
+                        isFindResult = true
+                    }
+                }
+                .padding(.top, 8)
+                if isWarning && mbti == nil {
                     Text("MBTI를 선택해 주세요")
-                        .applyFontStyle(.subtitle)
-                        .padding()
+                        .foregroundColor(.red)
+                        .applyFontStyle(.label)
+                        .padding(.top, 4)
                 }
+                
+                Spacer()
             }
+            Spacer()
         }
         .background(Color.main100)
-        .ignoresSafeArea()
     }
 }
