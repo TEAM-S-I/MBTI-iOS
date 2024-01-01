642da877941b03ca1d0d0325d83df48c763bad5c
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-01 14:07:58 +0900

    Fix :: dummy to real in find result view

--- mbti/mbti/Source/Feature/FindResult/Component/FindResultCeil.swift
+++ mbti/mbti/Source/Feature/FindResult/Component/FindResultCeil.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct FindResultCeil: View {
+    
+    let title: String
+    let subTitle: String
+    let strengths: String
+    let weaknesses: String
+    
     var body: some View {
         HStack(spacing: 0) {
             Spacer()
             VStack(spacing: 0) {
-                NameCircle("MBTI", type: .green)
+                NameCircle(title, type: .green)
                     .padding(.top, 24)
-                Text("타고난 리더")
+                Text(subTitle)
                     .applyFontStyle(.title)
                     .padding(.top, 8)
                 
-                Text("MBTI와 이런 부분들이 잘 맞아요!")
+                Text("\(title)님과 이런 부분들이 잘 맞아요!")
                     .applyFontStyle(.subtitle)
                     .padding(.leading, 20)
                     .padding(.top, 32)
@@ -29,7 +35,7 @@ struct FindResultCeil: View {
                     .padding(.top, 12)
                     .padding(.leading, 16)
                     .toLeading()
-                Text("MBTI와 보완해야 할 것들이에요")
+                Text("\(title)님과 보완해야 할 것들이에요")
                     .applyFontStyle(.subtitle)
                     .padding(.leading, 20)
                     .padding(.top, 72)
