338739cce5971abe9e681a52a8a573d64052d0da
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-28 11:24:37 +0900

    fix : swipe back

--- mbti/mbti/Source/Extension/ViewExt.swift
+++ mbti/mbti/Source/Extension/ViewExt.swift
@@ -13,6 +13,18 @@ extension View {
     }
 }
 
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+
+
 
 extension View {
     func addMbtiLogo() -> some View {
